ad25bf4bd76d4c236bb29819b06e5f853c06d89e
Павел <pavel.boltonosov@gmail.com>
2025-11-18 18:59:16 +0300

    First commit in repository

--- src/StringProcessing/Task1.java
+++ src/StringProcessing/Task1.java
@@ -3,6 +3,31 @@ package StringProcessing;
 public class Task1 {
     public static void main(String[] args) {
 
+        String str = "I like Java!!!";
+
+        char lastchar = str.charAt(str.length() - 1);
+        System.out.println(lastchar);
+
+        boolean endsWith = str.endsWith("!");
+        System.out.println(endsWith);
+
+        boolean startsWith = str.startsWith("I like");
+        System.out.println(startsWith);
+
+        boolean contains = str.contains("Java");
+        System.out.println(contains);
+
+        int position = str.indexOf("Java");
+        System.out.println(position);
+
+        String replace = str.replace("a", "o");
+        System.out.println(replace);
+
+        String upperCase = str.toUpperCase();
+        System.out.println(upperCase);
+
+        String lowerCase = str.toLowerCase();
+        System.out.println(lowerCase);
 
     }
 }
